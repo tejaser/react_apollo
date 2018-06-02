1ba4700a1078917bba539cf47fcf23c0e75d32b0
Tejas Pancholi <tejas.er@outlook.com>
2018-06-02 20:07:14 +0530

    first actual query.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,24 +1,3 @@
 // console.log('Hello, from the console!!');
-import { createApolloServer } from 'meteor/apollo';
-import { makeExecutableSchema } from 'graphql-tools';
 
-const typeDefs = `
-  type Query {
-    hi: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hi() {
-      return 'Hello, From Apollo!';
-    }
-  }
-};
-
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers
-});
-
-createApolloServer({ schema });
+import './register-api';
