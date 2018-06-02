95eb01d5960a1308b80dc56ae0f5fb44b13fbdca
Tejas Pancholi <tejas.er@outlook.com>
2018-06-02 18:41:46 +0530

    setup apollo server.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,1 +1,24 @@
-console.log('Hello, from the console!!');
+// console.log('Hello, from the console!!');
+import { createApolloServer } from 'meteor/apollo';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const typeDefs = `
+  type Query {
+    hi: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hi() {
+      return 'Hello, From Apollo!';
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({ schema });
